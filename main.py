4746ea700830b54046b784713186adcf8fb49f1f
Aaushi Agrawal <117663598+aaushi-bu@users.noreply.github.com>
2023-12-04 10:22:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
+import pandas as pd
 import openai
 import os
-from deep_translator import GoogleTranslator
+from googletrans import Translator
 
 # Set up the OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
@@ -25,12 +26,6 @@ def generate_lyrics(artist_name, genre, temperature=0.7, use_slang=False):
     generated_lyric = response['choices'][0]['text']
     return generated_lyric
 
-# Function to translate lyrics to Hindi
-def translate_to_hindi(text):
-    translator = GoogleTranslator()
-    translation = translator.translate(text, dest='hi')
-    return translation.text
-
 # Streamlit app
 st.title("Lyric Generator Chatbot")
 
@@ -39,7 +34,6 @@ artist_name = st.text_input("Enter the artist's name:")
 genre = st.text_input("Enter the genre:")
 temperature = st.slider("Select temperature", 0.1, 1.0, 0.7, 0.1)
 use_slang = st.checkbox("Allow Slang in Lyrics", value=False, key='slang_checkbox', help='Use slang and casual language in the lyrics.')
-translate_to_hindi_checkbox = st.checkbox("Translate to Hindi", value=False, key='translate_checkbox', help='Translate the generated lyrics to Hindi.')
 
 # Generate lyrics when the user clicks the button
 if st.button("Generate Lyrics"):
@@ -47,11 +41,8 @@ if st.button("Generate Lyrics"):
         # Call the generate_lyrics function
         generated_lyric = generate_lyrics(artist_name, genre, temperature, use_slang)
 
-        # Translate to Hindi if the checkbox is selected
-        if translate_to_hindi_checkbox:
-            translated_lyric = translate_to_hindi(generated_lyric)
-            st.success(f"Generated Lyric (Translated to Hindi):\n{translated_lyric}")
-        else:
-            st.success(f"Generated Lyric:\n{generated_lyric}")
+   
+        # Display the generated or translated lyric
+        st.success(f"Generated Lyric:\n{generated_lyric}")
     else:
         st.warning("Please fill in the artist's name and genre.")
