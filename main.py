03baff313c47a8a9fbe86ba37c6850a267aeade6
amrithafarswani <122059440+amrithafarswani@users.noreply.github.com>
2023-12-04 09:01:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,29 +27,14 @@ def generate_lyrics(artist_name, genre, temperature=0.7, use_slang=False):
     generated_lyric = response['choices'][0]['text']
     return generated_lyric
 
-# Function to translate text from English to German
-def translate_to_german(text):
-    translator = Translator()
-    translation = translator.translate(text, dest='de')
-    return translation.text
-
 # Streamlit app
 st.title("Lyric Generator Chatbot")
 
 # Get user inputs
 artist_name = st.text_input("Enter the artist's name:")
 genre = st.text_input("Enter the genre:")
-subject = st.text_input("Subject:", "Enter the subject for this particular song")
-rhyme = st.text_input("Rhyme:", "Enter a particular word or phrase that you would like used")
 temperature = st.slider("Select temperature", 0.1, 1.0, 0.7, 0.1)
 use_slang = st.checkbox("Allow Slang in Lyrics", value=False, key='slang_checkbox', help='Use slang and casual language in the lyrics.', use_container_width=True)
-
-# Toggle button for translation
-translate_toggle = st.checkbox("Translate to German", value=False, key='translate_toggle', help='Toggle to translate the generated lyric to German.')
-
-# Logging statements to check the values of use_slang and translate_toggle
-st.write(f"use_slang value: {use_slang}")
-st.write(f"translate_toggle value: {translate_toggle}")
 
 # Generate lyrics when the user clicks the button
 if st.button("Generate Lyrics"):
@@ -57,11 +42,8 @@ if st.button("Generate Lyrics"):
         # Call the generate_lyrics function
         generated_lyric = generate_lyrics(artist_name, genre, subject, rhyme, temperature, use_slang)
 
-        # Translate the generated lyric to German if the toggle is on
-        translated_lyric = translate_to_german(generated_lyric) if translate_toggle else generated_lyric
-
+   
         # Display the generated or translated lyric
-        st.success(f"Generated Lyric (English):\n{generated_lyric}")
-        st.success(f"Translated Lyric (German):\n{translated_lyric}")
+        st.success(f"Generated Lyric:\n{generated_lyric}")
     else:
         st.warning("Please fill in the artist's name and genre.")
