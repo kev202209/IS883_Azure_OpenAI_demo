769d9897c3ef578b12553ecb936a188db6983eee
Aaushi Agrawal <117663598+aaushi-bu@users.noreply.github.com>
2023-12-04 11:18:49 -0500

    Update main.py with appropirate slang prompt

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Function to generate lyrics
 def generate_lyrics(artist_name, genre, subject=None, rhyme=None, temperature=0.7, use_slang=False):
-    prompt = f"Write the lyrics to a song based on this {genre} that the author wants: {subject}, in similarity to this artist: {artist_name}, and if available create rhymes with this phrase {rhyme}"
+    prompt = f"Imagine you are a songwriter. Write the lyrics to a song based on this {genre} that the author wants: {subject}, in similarity to this artist: {artist_name}, and if available create rhymes with this phrase {rhyme}. Try your best to match the style of the artist. Unless specified, do not use slang or casual langauge in the lyrics generated."
 
     # Modify the prompt based on the use_slang parameter
     if use_slang:
-        prompt += " Use slang and casual language in the lyrics."
+        prompt += " You are allowed to use slang and casual language in the lyrics in this case."
 
     # Generate lyrics using OpenAI GPT-3
     response = openai.Completion.create(
